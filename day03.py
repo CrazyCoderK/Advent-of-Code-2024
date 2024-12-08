5264858f08f1448a682a6a8a79b8c36645593712
Aarav Kushagra <68432299+CrazyCoderK@users.noreply.github.com>
2024-12-08 17:44:39 +0000

    Update day03.py

--- day03.py
+++ day03.py
@@ -9,22 +9,19 @@ with open("AoC/2024\input.txt", "r") as f:
 def part1(data):
     l = re.findall("mul\(\d+,\d+\)", data)
     ans1 = 0
-    ans1 += [int(e.split(",")[0][4:]) * int(e.split(",")[1][:-1]) for e in l]
+    for item in l:
+        ans1 += int(item.split(",")[0][4:]) * int(item.split(",")[1][:-1])
     return ans1
 
 def part2(data):
     ans2 = 0
-    # for part 2 i ended up putting a "do()" at the very start of my input just to make parsing it way easier)
     l = re.findall("(do\(\)|mul\(\d+,\d+\)|don't\(\))", data)
     enabled = True
     for item in l:
         if "do(" in item:
             enabled = True
         if "mul" in item and enabled == True:
-            x = item.split(",")
-            n1 = int(x[0][4:])
-            n2 = int(x[1][:-1])
-            ans2 += n1 * n2
+            ans2 += int(item.split(",")[0][4:]) * int(item.itemsplit(",")[1][:-1])
         if "don" in item:
             enabled = False
     return ans2
